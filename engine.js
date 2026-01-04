8cf00d4b6dc1b7f81a63291ecb7dcc0a97e66af4
brunomoraesdigital <brunomoraes.dev@gmail.com>
2026-01-04 00:36:09 -0300

    update engine.js

--- engine.js
+++ engine.js
@@ -9,15 +9,50 @@ const fim = {
     y: areaDoJogo.height
 };
 
+const coordAreaJogo = { inicio, fim };
+
 const contexto = areaDoJogo.getContext('2d');
 
+/* ------------- */
+
 const alturaChao = 100;
-const alturaPersonagem = 50;
-const larguraPersonagem = alturaPersonagem;
+const gravidade = 0.5;
 
-let personagemPosY = 0;
-let velocidadeY = 0;
+let personagem = {
+    cor: 'blue',
+    altura: 50,
+    largura: 50,
+    posX: 160,
+    posY: 0,
+    velX: 0,
+    velY: 0,
+    colisao: function() {
+        return {
+            ldEsq: this.posX,
+            ldDir: this.posX + this.largura,
+            ldTopo: this.posY,
+            ldBase: this.posY + this.altura
+        };
+    }
+};
 
+let chao = {
+    cor: 'green',
+    altura: alturaChao,
+    largura: coordAreaJogo.fim.x,
+    posX: coordAreaJogo.inicio.x,
+    posY: coordAreaJogo.fim.y - alturaChao,
+    velX: 0,
+    velY: 0,
+    colisao: function() {
+        return {
+            ldEsq: this.posX,
+            ldDir: this.posX + this.largura,
+            ldTopo: this.posY,
+            ldBase: this.posY + this.altura
+        };
+    }
+}
 
 function desenhar(cor, x, y, largura, altura) {
     contexto.fillStyle = cor;
@@ -25,37 +60,44 @@ function desenhar(cor, x, y, largura, altura) {
 }
 
 function atualizarFisica(gravidade) {
-    velocidadeY = velocidadeY + gravidade;
-    personagemPosY = personagemPosY + velocidadeY;
-
+    personagem.velY += gravidade;
+    personagem.posY += personagem.velY;
 }
 
 function verificarColisao() {
-    if (personagemPosY + alturaPersonagem > (fim.y - alturaChao)) {
-        personagemPosY = (fim.y - alturaChao) - alturaPersonagem;
-        velocidadeY = 0;
+    
+    let limitesPersonagem = personagem.colisao();
+    let limitesChao = chao.colisao();
+    
+    if (limitesPersonagem.ldBase > limitesChao.ldTopo) {
+        personagem.posY = limitesChao.ldTopo - personagem.altura;
+        personagem.velY = 0;
+    }
+    if (true) {
+        console.log("foi");
+        personagem.velY += gravidade;
+    personagem.posY += -personagem.velY;
     }
 }
 
 function loopPrincipal() {
-
+    
     // 1️⃣ limpar o canvas
     contexto.clearRect(0, 0, fim.x, fim.y);
-
+    
     // 2️⃣ desenhar o retângulo (ou outros elementos do jogo)
-
-    //contexto.fillRect(0, 540, 360, 100);
-    desenhar('green', inicio.x, fim.y - alturaChao, fim.x, alturaChao);
-    desenhar('blue', 160, personagemPosY, larguraPersonagem, alturaPersonagem)
+    
+    desenhar(chao.cor, chao.posX, chao.posY, chao.largura, chao.altura);
+    desenhar(personagem.cor, personagem.posX, personagem.posY, personagem.largura, personagem.altura);
     
     // 3️⃣ atualizar posições, lógica, colisões
-
+    
     atualizarFisica(0.5);
     verificarColisao();
-
+    
     // 4️⃣ pedir o próximo frame
     requestAnimationFrame(loopPrincipal);
-
+    
 }
 
 loopPrincipal();
